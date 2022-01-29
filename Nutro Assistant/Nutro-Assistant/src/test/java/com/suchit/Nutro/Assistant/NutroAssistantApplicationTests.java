b12bc7ac707eab11ac86b71547fb51c1b8e2b88d
Suchit V <88663662+VSuchit@users.noreply.github.com>
2022-01-29 14:57:52 +0530

    Add files via upload

---
+++ Nutro Assistant/Nutro-Assistant/src/test/java/com/suchit/Nutro/Assistant/NutroAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suchit.Nutro.Assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutroAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
